d1ef792b6737bff828c1a4362ca7ae3ac2fc8294
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-02-16 12:35:35 +0300

    A course grade calculator has been created in the 4-point system, just like in the university.It asks the user for the number of courses and grades and then finds the gano value.

--- src/CalculateGradeAverage.java
+++ src/CalculateGradeAverage.java
@@ -3,50 +3,42 @@ import java.util.Scanner;
 public class CalculateGradeAverage {
     public static void main(String[] args) {
 
-        // We are want to learn users points so we use Scanner and then we convert to double type them.
-        Scanner point = new Scanner(System.in);
-        System.out.println("Enter Your First Math Quiz Point?");
-        int firstMathQuiz = point.nextInt();
-        System.out.println("Enter Your Second Math Quiz Point?");
-        int secondMathQuiz = point.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter Your First Physics Quiz Point?");
-        int firstPhysicsQuiz = point.nextInt();
-        System.out.println("Enter Your Second Physics Quiz Point?");
-        int secondPhysicsQuiz = point.nextInt();
+        // How many do you lessons?
+        System.out.println("Enter the number of courses:");
+        int numberOfCourses = scanner.nextInt();
 
-        System.out.println("Enter Mathematical Lessons Hours");
-        int mathHours = point.nextInt();
+        // // We give initial values for defining the credit points and credit hours.
+        double totalCreditPoints = 0;
+        double totalCreditHours = 0;
 
-        System.out.println("Enter Physics Lessons Hours");
-        double physicsHours = point.nextDouble();
+        // We create loops for each lesson
+        for (int i = 1; i <= numberOfCourses; i++) {
+            System.out.println("Enter Your First Quiz Point for Course " + i + "?");
+            int firstQuiz = scanner.nextInt();
 
-        // This function is our main function. It find general average.
-        System.out.println(calculate(firstMathQuiz, secondMathQuiz, firstPhysicsQuiz, secondPhysicsQuiz, mathHours,physicsHours));
+            System.out.println("Enter Your Second Quiz Point for Course " + i + "?");
+            int secondQuiz = scanner.nextInt();
 
+            System.out.println("Enter Credit Hours for Course " + i + "?");
+            double creditHours = scanner.nextDouble();
+
+            // Calculate Credit Point and Course Average for everyone.
+            double courseAverage = (firstQuiz * 0.4) + (secondQuiz * 0.6);
+            double courseCreditPoint = creditPointCalculate(courseAverage);
+
+            // Update total credit point and credit hours
+            totalCreditPoints += courseCreditPoint * creditHours;
+            totalCreditHours += creditHours;
+        }
+
+        // Calculate General Grade Average
+        double generalAverageGrade = totalCreditPoints / totalCreditHours;
+        System.out.println("General Average Grade: " + generalAverageGrade);
     }
 
-    public static double calculate(double firstMathQuiz, double secondMathQuiz,
-                                   double firstPhysicsQuiz, double secondPhysicsQuiz,
-                                   double mathHours, double physicsHours) {
-
-        // We get 40% of the midterm and 60% of the final and we determine lessons average.
-        double mathAverage = ((firstMathQuiz * 0.4) + (secondMathQuiz * 0.6));
-        double physicsAverage = ((firstPhysicsQuiz * 0.4) + (secondPhysicsQuiz * 0.6));
-
-        // We find the corresponding score of each course in the 4-point system.
-        double mathCreditPoint = creditPointCalculate(mathAverage);
-        double physicsCreditPoint = creditPointCalculate(physicsAverage);
-
-
-        // To calculate the GPA, find the weighted average of each course according to the number of credits.
-        double generalAverageGrade = (mathCreditPoint * mathHours + physicsCreditPoint * physicsHours) / (mathHours + physicsHours);
-
-        return generalAverageGrade;
-
-    }
-
-    // Convert credit scores to 4-point system.
+    // We convert the system of 100 to the system of 4
     public static double creditPointCalculate(double point) {
         if (point >= 90) {
             return 4.0;
